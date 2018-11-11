73e27dc68f23a76fd61cf3354e9f54cf2baf79fe
Will Loftus <willcaleloftus@gmail.com>
2018-11-10 16:21:22 -0800

    updated backup

---
+++ backup/files/js/lobby-list.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#drop a").click(function() {
+        $("button span").html($(this).html());
+    });
+});
+
+function dropDown() {
+    document.getElementById("drop").classList.toggle("dropdown-visible");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches("#sort button")) {
+  
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("dropdown-visible")) {
+          openDropdown.classList.remove("dropdown-visible");
+        }
+      }
+    }
+  }
