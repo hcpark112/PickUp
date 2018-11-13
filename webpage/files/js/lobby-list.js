f7043c06187c6275c40e565ad2fabe3598d3fca7
Will Loftus <willcaleloftus@gmail.com>
2018-11-12 20:06:21 -0800

    created js function for creating the lobby boxes

--- webpage/files/js/lobby-list.js
+++ webpage/files/js/lobby-list.js
@@ -1,11 +1,61 @@
 $(document).ready(function() {
 
+  numLobby(12);
+
   //replaces drop down button text with selection
   $("#drop a").click(function() {
       $("#sort button").html($(this).html());
     });
 
 });
+
+//creates a random number of lobbies if no argument given,
+//else creates a lobby argument[0] number of times
+function numLobby() {
+  if(arguments[0] == null) {
+    var rand = ((Math.random() * 20) + 6) | 0;
+
+    for(var i = 0; i < rand; i++) {
+      createLobby();
+    }
+  } else {
+    for(var i = 0; i < arguments[0]; i++) {
+      createLobby();
+    }
+  }
+}
+
+//creates a lobby
+function createLobby() {
+  var lobbyDiv = $("<div class = 'lobby'></div>");
+  $("#lobby-list-content").append(lobbyDiv);
+
+  var boxDiv = $("<div class = 'box'></div>");
+  $(lobbyDiv).append(boxDiv);
+
+  var eventDateDiv = $("<div class = 'event-date'></div>");
+  $(boxDiv).append(eventDateDiv);
+  $(eventDateDiv).append("<img src = './files/images/calendar.png' alt = 'calendar'><div>January 1, 2019</div>");
+
+  var eventLocationDiv = $("<div class = 'event-location'></div>");
+  $(boxDiv).append(eventLocationDiv);
+  $(eventLocationDiv).append("<img src = './files/images/mapmarker.png' alt = 'mapmarker'><div>Trout Lake Park</div>");
+
+  var boxDescriptionDiv = $("<div class = 'box-description'></div>");
+  $(lobbyDiv).append(boxDescriptionDiv);
+
+  $(boxDescriptionDiv).append("<div class = 'box-profile'><img src = './files/images/person.png' alt = 'pic'></div>");
+  $(boxDescriptionDiv).append("<div class = 'user-name'>Username</div>");
+  $(boxDescriptionDiv).append("<div class = 'capacity'>12/24</div>");
+}
+
+function randDate() {
+  //creates a random date for the lobby
+}
+
+function randSize() {
+  //creates a random player size for the lobby
+}
 
 //toggles drop down when button is clicked
 function dropDown() {
