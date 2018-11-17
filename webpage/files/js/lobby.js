cdf62c00136e37ad55ad58750015ce9c513a44d3
hcpark112 <hpark112@my.bcit.ca>
2018-11-17 11:09:37 -0800

    Removed chat and chaged to rooms. Changed related css, js, html

--- webpage/files/js/lobby.js
+++ webpage/files/js/lobby.js
@@ -37,59 +37,29 @@ var chatcount = 0;
     }
   }
 
-  function friends(name, status){
-    var friend = $("<div class='friend'></div>");
-    $(friend).attr("ondblclick", "window.location.href='https://www.google.com'");
+  function lobby(name, sport, image_src){
+    var lobby = $("<div class='lobby'></div>");
+    $(lobby).attr("ondblclick", "window.location.href='https://www.google.com'");
 
-    var stat = $("<div class='stat'></div>");
-
-    var online = {"background-color": "green"};
-    var offline = {"background-color": "red"};
+    var image = $("<img class='roomicon'>");
+    $(image).attr("src", image_src);
+    $(image).attr("alt", sport);
 
     var namediv = $("<div class='namediv'></div>");
-    var username = $("<span class='username'></span>");
-    $(username).html(name);
+    var roomname = $("<span class='roomname'></span>");
+    $(roomname).html(name + "'s lobby");
 
-    if(status == "online"){
-      $(stat).css(online);
-        $("#leftbar").prepend(friend);
-    } else {
-      $(stat).css(offline);
-        $("#leftbar").append(friend);
-    }
-
-    $(friend).append(stat);
-    $(friend).append(namediv);
-    $(namediv).append(username);
-  }
-
-  function chats(name, chattext){
-    chatcount++;
-    var chatDiv = $("<div class='chat'></div>");
-    var chatcontent = $("<p></p>");
-
-    var textform = $("<form class='textform'></form>");
-    var textinput = $("<input class='textinput' type='text' name='text' placeholder='Enter Text' autocomplete='off'/>");
-    var textsubmit = $("<input class='textinput' type='image' name='submit' src='./files/images/enter.png' alt='enter'/>");
-
-    $(textform).append(textinput);
-    $(textform).append(textsubmit);
-    $(chatDiv).append(textform);
-
-    $(chatcontent).html(chattext);
-
-    var button = $("<button class='collapsible'></button>");
-    $(button).html(name);
-
-    $("#friendstab").append(chatDiv);
-    $(chatDiv).append(chatcontent);
-    $("#friendstab").append(button);
+    $("#leftbar").append(lobby);
+    // $(lobby).append(image);
+    $(lobby).append(namediv);
+    $(lobby).prepend(image);
+    $(namediv).append(roomname);
   }
 
   $(document).ready(function() {
     $("#slideshow > div:gt(0)").hide();
 
-    setInterval(function() { 
+    setInterval(function() {
       $('#slideshow > div:first')
         .fadeOut(0)
         .next()
@@ -100,37 +70,12 @@ var chatcount = 0;
 
 
 
-    friends("Alec Martin", "online");
-    friends("Will Loftus", "offline");
-    friends("Stella Tran", "offline");
-    friends("Jeavin Shoker", "offline");
-    friends("Emilio Ditrocchio", "online");
-    friends("Edward Lee", "online");
-    friends("Kevin Park", "online");
-
-    chats("Kevin", "Kevin: Hey, how's it going?");
-
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.previousElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      });
-    }
+    lobby("Will Loftus", "Basketball", "./files/images/basketball.png");
+    lobby("Jeavin Shoker", "Soccer", "./files/images/soccerball.png");
+    lobby("Emilio Ditrocchio", "Baseball", "./files/images/baseball.png");
 
     var addlink = $("<a></a>");
     $(addlink).attr("href", "https://www.google.com");
-
-    var addicon = $("<img id='addicon' alt='Add Friend' src='./files/images/addfriendwhite.png'>");
-    $("#friendstab").append(addlink);
-    $(addlink).append(addicon);
 
     partymember("Kevin", "5000", "teamone");
     partymember("Will", "5000", "teamtwo");
--- webpage/files/js/mainpage.js
+++ webpage/files/js/mainpage.js
@@ -1,52 +1,22 @@
 var chatcount = 0;
 
-  function friends(name, status){
-    var friend = $("<div class='friend'></div>");
-    $(friend).attr("ondblclick", "window.location.href='https://www.google.com'");
+  function lobby(name, sport, image_src){
+    var lobby = $("<div class='lobby'></div>");
+    $(lobby).attr("ondblclick", "window.location.href='https://www.google.com'");
 
-    var stat = $("<div class='stat'></div>");
-
-    var online = {"background-color": "green"};
-    var offline = {"background-color": "red"};
+    var image = $("<img class='roomicon'>");
+    $(image).attr("src", image_src);
+    $(image).attr("alt", sport);
 
     var namediv = $("<div class='namediv'></div>");
-    var username = $("<span class='username'></span>");
-    $(username).html(name);
+    var roomname = $("<span class='roomname'></span>");
+    $(roomname).html(name + "'s lobby");
 
-    if(status == "online"){
-      $(stat).css(online);
-        $("#leftbar").prepend(friend);
-    } else {
-      $(stat).css(offline);
-        $("#leftbar").append(friend);
-    }
-
-    $(friend).append(stat);
-    $(friend).append(namediv);
-    $(namediv).append(username);
-  }
-
-  function chats(name, chattext){
-    chatcount++;
-    var chatDiv = $("<div class='chat'></div>");
-    var chatcontent = $("<p></p>");
-
-    var textform = $("<form class='textform'></form>");
-    var textinput = $("<input class='textinput' type='text' name='text' placeholder='Enter Text' autocomplete='off'/>");
-    var textsubmit = $("<input class='textinput' type='image' name='submit' src='./files/images/enter.png' alt='enter'/>");
-
-    $(textform).append(textinput);
-    $(textform).append(textsubmit);
-    $(chatDiv).append(textform);
-
-    $(chatcontent).html(chattext);
-
-    var button = $("<button class='collapsible'></button>");
-    $(button).html(name);
-
-    $("#friendstab").append(chatDiv);
-    $(chatDiv).append(chatcontent);
-    $("#friendstab").append(button);
+    $("#leftbar").append(lobby);
+    // $(lobby).append(image);
+    $(lobby).append(namediv);
+    $(lobby).prepend(image);
+    $(namediv).append(roomname);
   }
 
   function category(sport, image_src, target){
@@ -76,7 +46,7 @@ var chatcount = 0;
   $(document).ready(function() {
     $("#slideshow > div:gt(0)").hide();
 
-    setInterval(function() { 
+    setInterval(function() {
       $('#slideshow > div:first')
         .fadeOut(0)
         .next()
@@ -87,29 +57,9 @@ var chatcount = 0;
 
 
 
-    friends("Alec Martin", "online");
-    friends("Will Loftus", "offline");
-    friends("Stella Tran", "offline");
-    friends("Jeavin Shoker", "offline");
-    friends("Emilio Ditrocchio", "online");
-    friends("Edward Lee", "online");
-
-    chats("Kevin", "Kevin: Hey, how's it going?");
-
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.previousElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      });
-    }
+    lobby("Will Loftus", "Basketball", "./files/images/basketball.png");
+    lobby("Jeavin Shoker", "Soccer", "./files/images/soccerball.png");
+    lobby("Emilio Ditrocchio", "Baseball", "./files/images/baseball.png");
 
     category("Basketball", "./files/images/basketball.png", "https://www.google.com");
     category("Soccer", "./files/images/soccerball.png", "https://www.google.com");
@@ -117,14 +67,10 @@ var chatcount = 0;
     category("Baseball", "./files/images/baseball.png", "https://www.google.com");
     category("Volleyball", "./files/images/volleyball.png", "https://www.google.com");
 
-    
+
 
 
     var addlink = $("<a></a>");
     $(addlink).attr("href", "https://www.google.com");
-
-    var addicon = $("<img id='addicon' alt='Add Friend' src='./files/images/addfriendwhite.png'>");
-    $("#friendstab").append(addlink);
-    $(addlink).append(addicon);
 
   });
