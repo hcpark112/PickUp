00651d61a9ec20fa3055a3657120d4766cf50cf2
hcpark112 <hpark112@my.bcit.ca>
2018-11-22 22:37:40 -0800

    added pull methods to index. Changed lobby to use DB and it's css. Removed leftbar

--- backup/files/js/mainpage.js
+++ backup/files/js/mainpage.js
@@ -1,130 +1,69 @@
-var chatcount = 0;
+function lobby(name, sport, image_src){
+    var lobby = $("<div class='lobby'></div>");
+    $(lobby).attr("ondblclick", "window.location.href='https://www.google.com'");
 
-  function friends(name, status){
-    var friend = $("<div class='friend'></div>");
-    $(friend).attr("ondblclick", "window.location.href='https://www.google.com'");
-
-    var stat = $("<div class='stat'></div>");
-
-    var online = {"background-color": "green"};
-    var offline = {"background-color": "red"};
-
-    var namediv = $("<div class='namediv'></div>");
-    var username = $("<span class='username'></span>");
-    $(username).html(name);
-
-    if(status == "online"){
-      $(stat).css(online);
-        $("#leftbar").prepend(friend);
-    } else {
-      $(stat).css(offline);
-        $("#leftbar").append(friend);
-    }
-
-    $(friend).append(stat);
-    $(friend).append(namediv);
-    $(namediv).append(username);
-  }
-
-  function chats(name, chattext){
-    chatcount++;
-    var chatDiv = $("<div class='chat'></div>");
-    var chatcontent = $("<p></p>");
-
-    var textform = $("<form class='textform'></form>");
-    var textinput = $("<input class='textinput' type='text' name='text' placeholder='Enter Text' autocomplete='off'/>");
-    var textsubmit = $("<input class='textinput' type='image' name='submit' src='./files/images/enter.png' alt='enter'/>");
-
-    $(textform).append(textinput);
-    $(textform).append(textsubmit);
-    $(chatDiv).append(textform);
-
-    $(chatcontent).html(chattext);
-
-    var button = $("<button class='collapsible'></button>");
-    $(button).html(name);
-
-    $("#friendstab").append(chatDiv);
-    $(chatDiv).append(chatcontent);
-    $("#friendstab").append(button);
-  }
-
-  function category(sport, image_src, target){
-    var catediv = $("<div class='catediv'></div>");
-    var newlink = $("<a class='newlink'></a>");
-    $(newlink).attr("href", target);
-
-    var cate = $("<div class='cate'></div>");
-
-    var image = $("<img class='icon'>");
+    var image = $("<img class='roomicon'>");
     $(image).attr("src", image_src);
     $(image).attr("alt", sport);
 
-    var sportname = $("<p class='sportname'></p>");
-    $(sportname).html(sport);
-    var namelink = $("<a class='namelink'></a>");
-    $(namelink).attr("href", target);
+    var namediv = $("<div class='namediv'></div>");
+    var roomname = $("<span class='roomname'></span>");
+    $(roomname).html(name + "'s lobby");
 
-    $("#categories").append(catediv);
-    $(catediv).append(newlink);
-    $(newlink).append(cate);
-    $(cate).append(image);
-    $(catediv).append(namelink);
-    $(namelink).append(sportname);
+    $("#leftbar").append(lobby);
+    // $(lobby).append(image);
+    $(lobby).append(namediv);
+    $(lobby).prepend(image);
+    $(namediv).append(roomname);
   }
 
-  $(document).ready(function() {
-    $("#slideshow > div:gt(0)").hide();
+function category(sport, image_src, target){
+  var catediv = $("<div class='catediv'></div>");
+  var newlink = $("<a class='newlink'></a>");
+  $(newlink).attr("href", target);
 
-    setInterval(function() { 
-      $('#slideshow > div:first')
-        .fadeOut(0)
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('#slideshow');
-    },  15000);
+  var cate = $("<div class='cate'></div>");
 
+  var image = $("<img class='icon'>");
+  $(image).attr("src", image_src);
+  $(image).attr("alt", sport);
 
+  var sportname = $("<p class='sportname'></p>");
+  $(sportname).html(sport);
+  var namelink = $("<a class='namelink'></a>");
+  $(namelink).attr("href", target);
 
-    friends("Alec Martin", "online");
-    friends("Will Loftus", "offline");
-    friends("Stella Tran", "offline");
-    friends("Jeavin Shoker", "offline");
-    friends("Emilio Ditrocchio", "online");
-    friends("Edward Lee", "online");
+  $("#categories").append(catediv);
+  $(catediv).append(newlink);
+  $(newlink).append(cate);
+  $(cate).append(image);
+  $(catediv).append(namelink);
+  $(namelink).append(sportname);
+}
 
-    chats("Kevin", "Kevin: Hey, how's it going?");
+$(document).ready(function() {
+  $("#slideshow > div:gt(0)").hide();
 
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(0)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  },  15000);
+  
+  lobby("Will Loftus", "Basketball", "./files/images/basketball.png");
+  lobby("Jeavin Shoker", "Soccer", "./files/images/soccerball.png");
+  lobby("Emilio Ditrocchio", "Baseball", "./files/images/baseball.png");
 
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.previousElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-      });
-    }
+  category("Basketball", "./files/images/basketball.png", "https://www.google.com");
+  category("Soccer", "./files/images/soccerball.png", "https://www.google.com");
+  category("Football", "./files/images/football.png", "https://www.google.com");
+  category("Baseball", "./files/images/baseball.png", "https://www.google.com");
+  category("Volleyball", "./files/images/volleyball.png", "https://www.google.com");
 
-    category("Basketball", "./files/images/basketball.png", "https://www.google.com");
-    category("Soccer", "./files/images/soccerball.png", "https://www.google.com");
-    category("Football", "./files/images/football.png", "https://www.google.com");
-    category("Baseball", "./files/images/baseball.png", "https://www.google.com");
-    category("Volleyball", "./files/images/volleyball.png", "https://www.google.com");
+  var addlink = $("<a></a>");
+  $(addlink).attr("href", "https://www.google.com");
 
-    
-
-
-    var addlink = $("<a></a>");
-    $(addlink).attr("href", "https://www.google.com");
-
-    var addicon = $("<img id='addicon' alt='Add Friend' src='./files/images/addfriendwhite.png'>");
-    $("#friendstab").append(addlink);
-    $(addlink).append(addicon);
-
-  });
+});
