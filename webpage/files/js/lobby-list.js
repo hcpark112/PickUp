205d9a92a054131c510dbdf9470cf3c8ee269024
Will Loftus <willcaleloftus@gmail.com>
2018-11-26 15:17:58 -0800

    Added sorts, organized javascript

--- webpage/files/js/lobby-list.js
+++ webpage/files/js/lobby-list.js
@@ -1,3 +1,9 @@
+/**
+ * JAVASCRIPT FOR lobbylist.html PAGE
+ *
+ * @author William Loftus, A01022399
+ */
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyD0bFMT_-dlAfllbPc2rJXhZRnrJIERwv8",
@@ -11,131 +17,48 @@ firebase.initializeApp(config);
 var dbRef = firebase.database();
 var gamesRef = firebase.database().ref().child('PickUp').child('Games');
 
+
+
+/*******************************************************************************
+*************************MISC JQUERY FUNCTIONS**********************************
+********************************************************************************/
+
+/**
+ * Contains JQuery click functions for different buttons on the page.
+ */
 $(document).ready(function() {
-  //replaces drop down button text with selection
+
+  /**Replaces drop-down text with selection from drop-down menu.*/
   $("#drop a").click(function() {
       $("#sort button").html($(this).html());
     });
+
+  /**Sorts the lobby divs by date.*/
+  $("#date").click(function() {
+    dateSort();
+  });
+
+  /**Sorts the lobby divs by max number of players.*/
+  $("#players").click(function() {
+    capacitySort();
+  })
+
+  /**No functionality yet, randomizes the order of the lobby divs.*/
+  $("#distance").click(function() {
+    randomize();
+  })
 });
 
-//creates a random number of lobbies if no argument given,
-//else creates a lobby argument[0] number of times
-function numLobby() {
-  if(arguments[0] == null) {
-    var rand = ((Math.random() * 20) + 6) | 0;
-
-    for(var i = 0; i < rand; i++) {
-      createLobby();
-    }
-  } else {
-    for(var i = 0; i < arguments[0]; i++) {
-      createLobby();
-    }
-  }
-}
-
-//creates a lobby
-function createLobby() {
-  var date = randDate();
-  var size = randSize();
-
-  var lobbyDiv = $("<div class = 'lobby'></div>");
-  $("#lobby-list-content").append(lobbyDiv);
-
-  var boxDiv = $("<div class = 'box'></div>");
-  $(lobbyDiv).append(boxDiv);
-
-  var eventDateDiv = $("<div class = 'event-date'></div>");
-  $(boxDiv).append(eventDateDiv);
-  $(eventDateDiv).append("<img src = './files/images/calendar.png' alt = 'calendar'><div>" + date + "</div>");
-
-  var eventLocationDiv = $("<div class = 'event-location'></div>");
-  $(boxDiv).append(eventLocationDiv);
-  $(eventLocationDiv).append("<img src = './files/images/mapmarker.png' alt = 'mapmarker'><div>Trout Lake Park</div>");
-
-  var boxDescriptionDiv = $("<div class = 'box-description'></div>");
-  $(lobbyDiv).append(boxDescriptionDiv);
-
-  $(boxDescriptionDiv).append("<div class = 'box-profile'><img src = './files/images/person.png' alt = 'pic'></div>");
-  $(boxDescriptionDiv).append("<div class = 'user-name'>Username</div>");
-  $(boxDescriptionDiv).append("<div class = 'capacity'>" + size + "</div>");
-}
-
-//creates a random date for the lobby
-function randDate() {
-
-  var month = monthConvert(((Math.random() * 12) + 1) | 0);
-  var day = ((Math.random() * 30) + 1) | 0;
-
-  return month + " " + day + ", 2019";
-}
-
-//converts a number to it's corresponding month
-function monthConvert(monthNum) {
-
-  var month;
-  switch(monthNum) {
-    case 1:
-      month = "January";
-      break;
-    case 2:
-      month = "February";
-      break;
-    case 3:
-      month = "March";
-      break;
-    case 4:
-      month = "April";
-      break;
-    case 5:
-      month = "May";
-      break;
-    case 6:
-      month = "June";
-      break;
-    case 7:
-      month = "July";
-      break;
-    case 8:
-      month = "August";
-      break;
-    case 9:
-      month = "September";
-      break;
-    case 10:
-      month = "October";
-      break;
-    case 11:
-      month = "November";
-      break;
-    case 12:
-      month = "December";
-      break;
-    default:
-      throw "Error: Not valid month number";
-  }
-
-  return month;
-}
-
-//creates a random player size for the lobby
-function randSize() {
-  var max;
-  do {
-    max = ((Math.random() * 10) + 4) | 0;
-  }
-  while(max % 2 != 0);
-  var curr = ((Math.random() * max)) | 0;
-
-  return "" + curr + "/" + max;
-}
-
-//toggles drop down when button is clicked
+/**
+ * Displays the drop-down menu when the button is clicked.
+ */
 function dropDown() {
     document.getElementById("drop").classList.toggle("dropdown-visible");
 }
 
-//closes drop down when user clicks somewhere else on the page
+/**
+ * Closes the drop-down menu when something else on the screen is clicked.
+ */
 window.onclick = function(event) {
     if (!event.target.matches("#sort button")) {
 
@@ -149,21 +72,23 @@ window.onclick = function(event) {
       }
     }
   }
-
-
-/******************************************************************************
-********************NEW JAVASCRIPT FOR FIREBASE INTEGRATION********************
+/*******************************************************************************
 ********************************************************************************/
 
-/*The array of lobby objects.*/
+
+
+/*******************************************************************************
+****************FUNCTIONS TO RETRIEVE DATA FROM FIREBASE************************
+********************************************************************************/
+
+/**The array of lobby objects.*/
 var lobbyArr = [];
 
-/*The max number of games in the data base, set to 2 for developing purposes*/
+/**The max number of games in the data base, set to 12 for testing purposes*/
 const MAX_GAMES = 12;
 
 /**
- * Retrieves the "games" branch of the firebase database. Then
- * passes the branch/snapshot into the getLobbyListData function.
+ *
  */
 gamesRef.on('child_added', function(snapshot) {
   let obj = getLobbyListData(snapshot);
@@ -176,10 +101,9 @@ gamesRef.on('child_added', function(snapshot) {
 });
 
 /**
- * Pulls the capacity and date data from each game. It then passes this
- * information to the createLobbyObj function.
  *
- * @param snapshot the "Games" branch containing list of all current games
+ * @param snapshot
+ * @return obj
  */
 function getLobbyListData(snapshot) {
   let hasDate = false, hasCapacity = false, hasOwner = false;
@@ -202,8 +126,8 @@ function getLobbyListData(snapshot) {
 
     if(hasDate && hasCapacity && hasOwner) {
       obj = {
-        maxSize: capacity,
-        date   : date,
+        maxSize: parseInt(capacity),
+        date   : parseDate(date),
         owner  : owner,
         html   : createLobbyHTML(date, capacity, owner)
       }
@@ -212,13 +136,21 @@ function getLobbyListData(snapshot) {
   });
   return obj;
 }
+/*******************************************************************************
+********************************************************************************/
 
-/*
- * Uses JQuery to create the HTML for a lobby div.
+
+
+/*******************************************************************************
+*************************FUNCTIONS FOR MAKING HTML******************************
+********************************************************************************/
+
+/**
  *
- * @param date the Date of the lobby.
- * @param capacity the max capacity of the lobby.
- * @returns the Completed Lobby Div, with a unique date and capacity.
+ * @param date
+ * @param capacity
+ * @param owner
+ * @return
  */
 function createLobbyHTML(date, capacity, owner) {
   let lobbyDiv = $("<div class = 'lobby'></div>");
@@ -244,6 +176,11 @@ function createLobbyHTML(date, capacity, owner) {
   return lobbyDiv;
 }
 
+/**
+ *
+ * @param date
+ * @return
+ */
 function formatDate(date) {
   let arr = date.split("/");
   let month = formatMonth(arr[0]), day = arr[1], year = arr[2];
@@ -255,6 +192,10 @@ function formatDate(date) {
   return month + " " + day + ", " + year;
 }
 
+/**
+ *
+ * @param month
+ */
 function formatMonth(month) {
   switch(month) {
     case "JAN":
@@ -282,18 +223,161 @@ function formatMonth(month) {
     case "DEC":
       return "December";
     default:
-      throw "Error: Not valid month number";
+      throw "Error: Not valid month";
   }
 }
 
+/**
+ *
+ * @param capacity
+ * @return
+ */
 function formatCapacity(capacity) {
   var currPlayers = ((Math.random() * capacity)) | 0;
   return currPlayers + "/" + capacity;
 }
 
-//grabs the contents of lobbyArr and appends the html of each obj to the page
+/**
+ *
+ */
 function appendToPage() {
   for(let i = 0; i < lobbyArr.length; i++) {
     $("#lobby-list-content").append(lobbyArr[i].html);
   }
 }
+/*******************************************************************************
+********************************************************************************/
+
+
+
+/*******************************************************************************
+********************HELPER FUNCTIONS FOR COMPARISONS****************************
+********************************************************************************/
+
+/**
+ *
+ * @param date
+ * @return
+ */
+function parseDate(date) {
+  let arr = date.split("/");
+  let monthStr = arr[0], dayStr = arr[1], yearStr = arr[2];
+
+  // console.log(parseInt(dayStr) + monthVal(monthStr) + yearVal(yearStr));
+  return parseInt(dayStr) + monthVal(monthStr) + yearVal(yearStr);
+
+}
+
+/**
+ *
+ * @param month
+ * @return
+ */
+function monthVal(month) {
+  switch(month) {
+    case "JAN":
+      return 100;
+    case "FEB":
+      return 200;
+    case "MAR":
+      return 300;
+    case "APR":
+      return 400;
+    case "MAY":
+      return 500;
+    case "JUN":
+      return 600;
+    case "JUL":
+      return 700;
+    case "AUG":
+      return 800;
+    case "SEP":
+      return 900;
+    case "OCT":
+      return 1000;
+    case "NOV":
+      return 1100;
+    case "DEC":
+      return 1200;
+    default:
+      throw "Error: Not valid month";
+  }
+}
+
+/**
+ *
+ * @param year
+ * @return
+ */
+function yearVal(year) {
+  switch(year) {
+    case "2018":
+      return 10000;
+    case "2019":
+      return 20000;
+    default:
+      return 0;
+  }
+}
+/*******************************************************************************
+********************************************************************************/
+
+
+
+/*******************************************************************************
+*******************************SORT FUNCTIONS***********************************
+********************************************************************************/
+
+/**
+ *
+ */
+function dateSort() {
+  for (let i = 1; i < lobbyArr.length; ++i) {
+    let temp = lobbyArr[i];
+    let j = i - 1;
+
+    while (j >= 0 && lobbyArr[j].date > temp.date) {
+      lobbyArr[j + 1] = lobbyArr[j];
+      j = j - 1;
+    }
+    lobbyArr[j + 1] = temp;
+  }
+  $(".lobby").remove();
+  appendToPage();
+}
+
+/**
+ *
+ */
+function capacitySort() {
+  for (let i = 1; i < lobbyArr.length; ++i) {
+    let temp = lobbyArr[i];
+    let j = i - 1;
+
+    while (j >= 0 && lobbyArr[j].maxSize > temp.maxSize) {
+      lobbyArr[j + 1] = lobbyArr[j];
+      j = j - 1;
+    }
+    lobbyArr[j + 1] = temp;
+  }
+  $(".lobby").remove();
+  appendToPage();
+}
+
+/**
+ *
+ */
+function randomize() {
+  let rand, temp;
+  for (let i = 0; i < lobbyArr.length; i++) {
+    rand = ((Math.random() * lobbyArr.length) ) | 0;
+
+    temp = lobbyArr[i];
+    lobbyArr[i] = lobbyArr[rand];
+    lobbyArr[rand] = temp;
+  }
+  $(".lobby").remove();
+  appendToPage();
+}
+/*******************************************************************************
+********************************************************************************/
