97192b3d97250bf128786e9dfaec83e035b63e3c
Alec Martin <acmartin1@live.ca>
2018-11-29 13:15:49 -0800

    Comments

--- webpage/files/js/LogIn.js
+++ webpage/files/js/LogIn.js
@@ -1,9 +1,37 @@
+    /**
+     * JAVASCRIPT FOR LogIn.html PAGE
+     *
+     * @author Alec Martin, A01059594
+     */
+    /**
+     * Creates Authentication constant for firebase
+     */
     const auth = firebase.auth();
+    /**
+     * Gets the text for email
+     */
     const txtEmail = document.getElementById('txtEmail');
+    /**
+     * Gets the text for password
+     */
     const txtPassword = document.getElementById('txtPassword');
+    /**
+     * Gets the input of the Log in button
+     */
     const input = document.querySelector('#mySubmit3');
+    /**
+     * Gets the input of the Password Form
+     */
     const input2 = document.querySelector('#txtPassword');
+    /**
+     * Checks if the Log in button has been pressed
+     */
     input.addEventListener('click', login);
+    /**
+     * Authenticates user if there Email and Password is correct
+     * else it returns an alert
+     * @param {} e 
+     */
       function login(e){
         e.preventDefault();
         const email = txtEmail.value;
@@ -11,7 +39,9 @@
         const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
         promise.catch(e => alert("Email and/or Password is incorrect"));
       }
-      
+      /**
+       * Checks if a enter has been pressed on the password form
+       */
       input2.addEventListener('keyup',function(e){
         if (e.keyCode === 13) {
             e.preventDefault();
@@ -21,24 +51,32 @@
             promise.catch(e => alert("Email and/or Password is incorrect"));
       }
     });
+    /**
+     * Makes a sleep function that is used for signing out
+     * @param {Milliseconds} ms 
+     */
       function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+    /**
+     * Makes the program wait before checking if the user is signed in
+     */
       async function wait() {
       await sleep(2000);
-      firebase.auth().onAuthStateChanged(firebaseUser =>{ 
-        if(firebaseUser) { 
-        console.log (firebaseUser); 
-        console.log("Logging in...");
-        document.location.href = "https://www.google.ca/";
-        } else { 
-        console.log('not logged in'); 
-        }
+      /**
+       * Checks if the user is signed in
+       */
+        firebase.auth().onAuthStateChanged(firebaseUser =>{ 
+          if(firebaseUser) { 
+          console.log (firebaseUser); 
+          console.log("Logging in...");
+          
+          } else { 
+          console.log('not logged in'); 
+          }
     });
 }
+     /**
+     * Makes the program wait before checking if the user is signed in
+     */
     wait();
-    var UsersRef = firebase.database().ref("Users/");
-    UsersRef.on("child_added", function(data) {
-        var newPlayer = data.val();
-        console.log("name: " + newPlayer.name);
-    });
